7b8e982f1a301b43142523f04316424252ef652c
Lev E <bearnode@gmail.com>
2020-01-15 19:34:35 +0300

    fix filename in build script

--- build_example.sh
+++ build_example.sh
@@ -4,4 +4,4 @@ tag="$(git describe --tags $(git rev-list --tags --max-count=1))"
 filename="pi-temp-$tag-arm64-linux"
 
 env GOOS=linux GOARCH=arm GOARM=6 go build -o release/pi-display example/*.go &&
-    cd release && tar -czvf $filename.tar.gz pi-temp
+    cd release && tar -czvf $filename.tar.gz pi-display
